5e7e8ab42d55ed39ece9254afc94a993c43a26d9
Itai Nathaniel <itainathaniel@gmail.com>
2025-03-05 11:57:06 +0200

    a reset

--- plugins/imcn-kafka/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { blueprintPluginEventsParams as blueprint, blueprintPluginEventsTypes, blueprintTypes } from "@amplication/code-gen-types";
-declare class BlueprintPluginTemplatePlugin implements blueprintTypes.AmplicationPlugin {
-    register(): blueprintPluginEventsTypes.BlueprintEvents;
-    beforeCreateBlueprint(context: blueprintTypes.DsgContext, eventParams: blueprint.CreateBlueprintParams): Promise<blueprint.CreateBlueprintParams>;
-}
-export default BlueprintPluginTemplatePlugin;
--- plugins/imcn-kafka/dist/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { PluginInstallation } from "@amplication/code-gen-types";
-import { Settings } from "./types";
-export declare const getPluginSettings: (pluginInstallations: PluginInstallation[]) => Settings;
